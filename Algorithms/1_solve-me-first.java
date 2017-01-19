e58da471ce94b750b19ed5eb085c6662ab896147
Darpan Shah <dshah22@users.noreply.github.com>
2017-01-19 17:25:13 -0500

    Create 1_solve-me-first.java

---
+++ Algorithms/1_solve-me-first.java
@@ -0,0 +1,28 @@
+/* Review the code provided in the editor below, then complete the solveMeFirst function 
+so that it returns the sum of two integers read from stdin.  */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int solveMeFirst(int a, int b) {
+         return a + b;
+
+   }
+
+   
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+   }
+}
